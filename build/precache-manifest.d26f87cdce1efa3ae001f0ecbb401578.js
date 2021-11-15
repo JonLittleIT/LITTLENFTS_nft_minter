5913b1227a3a5cd69be036103bcb39b1e3fa641a
Jon Little <jon@themodernlogicgroup.com>
2021-11-14 20:00:13 -0800

    adding files
    adding files

---
+++ build/precache-manifest.d26f87cdce1efa3ae001f0ecbb401578.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da3df77980fdec501fff8ccaf32f9d9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "270fbfecbfc03a24fd0e",
+    "url": "/static/css/main.74eda18f.chunk.css"
+  },
+  {
+    "revision": "04ff3e2622d59c2e8b72",
+    "url": "/static/js/2.60aa7f39.chunk.js"
+  },
+  {
+    "revision": "9141e6603b2ba65e9f944cb50ce12050",
+    "url": "/static/js/2.60aa7f39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2589ac0ef57ac79b9ab7",
+    "url": "/static/js/3.bf3e3a65.chunk.js"
+  },
+  {
+    "revision": "270fbfecbfc03a24fd0e",
+    "url": "/static/js/main.4a324fa9.chunk.js"
+  },
+  {
+    "revision": "b5175c81a44de6122244",
+    "url": "/static/js/runtime-main.a02f41c6.js"
+  },
+  {
+    "revision": "5db707bdba23196e8c50b4a92bdca350",
+    "url": "/static/media/1.5db707bd.png"
+  },
+  {
+    "revision": "027246ff63b670297128d95e26b96343",
+    "url": "/static/media/PressStart2P-Regular.027246ff.woff"
+  }
+]);
